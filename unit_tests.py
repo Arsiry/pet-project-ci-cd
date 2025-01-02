34c32b8b955863dbcb3ed3e4740558bc3be0e8bf
Anastasiia Trofymova <a-arsiry@ukr.net>
2025-01-02 18:52:04 -0500

    tests

--- unit_tests.py
+++ unit_tests.py
@@ -6,10 +6,13 @@ This module contains:
 """
 
 import unittest
-
+from unittest.mock import patch
+from io import StringIO
 
 # Import the function to be tested
 from project_sum_two_numbers import sum_of_two_numbers
+from project_sum_two_numbers.main import main
+
 
 class TestSumOfTwoNumbers(unittest.TestCase):
     """
@@ -35,6 +38,17 @@ class TestSumOfTwoNumbers(unittest.TestCase):
     def test_floats(self):
         """Test the sum of two floating-point numbers."""
         self.assertAlmostEqual(sum_of_two_numbers(3.5, 2.1), 5.6)
+
+
+class TestMainFunction(unittest.TestCase):
+    """
+    Test class for the main function.
+    """
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_main_output(self, mock_stdout):
+        """Test the output of the main function."""
+        main()
+        self.assertIn("The sum of 4 and 5 is 9.", mock_stdout.getvalue())
 
 
 if __name__ == "__main__":
