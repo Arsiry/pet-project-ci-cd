3e562a39fba11cfda60555f9d7b8580c4572351e
Anastasiia Trofymova <a-arsiry@ukr.net>
2025-01-09 11:59:11 -0500

    without packages

--- project_sum_two_numbers/__init__.py
+++ project_sum_two_numbers/__init__.py
@@ -3,4 +3,4 @@ Imports and makes the function sum_of_two_numbers from main.py accessible
 """
 
 from .main import sum_of_two_numbers
-from .main import read_and_display_file_content
+#from .main import read_and_display_file_content
