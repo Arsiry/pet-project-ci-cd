ec68d080b40b9ea6e06726786dd9a2e30e1f9374
Anastasiia Trofymova <a-arsiry@ukr.net>
2025-01-09 11:57:55 -0500

    without packages

--- project_sum_two_numbers/main.py
+++ project_sum_two_numbers/main.py
@@ -7,9 +7,6 @@ This script includes:
 - Reading from a file and displaying its contents.
 """
 
-import os
-import importlib.resources
-
 # Function to calculate the sum of two numbers
 def sum_of_two_numbers(a, b):
     """
@@ -24,23 +21,6 @@ def sum_of_two_numbers(a, b):
     """
     return a + b
 
-# Function to read from a file and display its content
-def read_and_display_file_content_from_package():
-    """
-    Reads and displays the content of 'sample.txt' from the package.
-
-    Returns:
-    None
-    """
-    try:
-        # Use importlib.resources to open the file from the package
-        with importlib.resources.open_text('project_sum_two_numbers.data', 'sample.txt') as file:
-            content = file.read()
-        print(content)
-    except FileNotFoundError:
-        print("File 'sample.txt' not found in the package.")
-
-
 def main():
     """
     Main function
@@ -50,9 +30,6 @@ def main():
     b = 5
     result = sum_of_two_numbers(a, b)
     print(f"The sum of {a} and {b} is {result}.")
-
-    # Read and display file content from the package
-    read_and_display_file_content_from_package()
 
 if __name__ == "__main__":
     main()
