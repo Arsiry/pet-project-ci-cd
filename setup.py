994ca143776faf98b61b220076e50bb96bb230fb
Anastasiia Trofymova <a-arsiry@ukr.net>
2025-01-09 10:30:44 -0500

    work with packages

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ setup(
     name='project_sum_two_numbers',
     version='0.1.0',
     packages=find_packages(),
-    include_package_data=True,
-    package_data={'': ['data/sample.txt']},
+    include_package_data=True,  
+    package_data={
+        'project_sum_two_numbers': ['data/sample.txt']  
+    },
     install_requires=[],
     entry_points={
         "console_scripts": [
